19bdc8c60d536b04ecf768fb621e3a954a6e1248
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-07-25 18:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(e)
+{   //Write your code here
+    e.preventDefault();
+    const radius = document.getElementById('radius').value;
+    if(radius < 0)
+    {
+        document.getElementById('volume').value = 'NaN';
+        return;
+    }
+    const volume = (4/3)*(Math.PI)*radius*radius*radius;
+    document.getElementById('volume').value = volume.toFixed(4);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
